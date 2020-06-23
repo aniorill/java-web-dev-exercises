dc785857fa9c075d6da1f9ded6d8744fdbcdadf3
aniorill <x.iorillo@gmail.com>
2020-06-23 19:58:44 -0400

    Prints word of desired length.

--- src/exercises/Ch3ArrayListPractice.java
+++ src/exercises/Ch3ArrayListPractice.java
@@ -1,11 +1,10 @@
 package exercises;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.Arrays;
-
 
 public class Ch3ArrayListPractice {
-    public static void main (String[] args){
+
+    public static void main(String[] args){
         int [] numbers = {1,2,3,4,5,6,7,8,9,10,11,12};
         int sum = 0;
         ArrayList<Integer> numberSum =  new ArrayList<Integer>();
@@ -15,11 +14,30 @@ public class Ch3ArrayListPractice {
         for (int i =0; i < numberSum.size(); i++) {
             if (numberSum.get(i) % 2 == 0){
                 sum += numberSum.get(i);
-                System.out.println(sum);
+                //System.out.println(sum);
             }
         }
-        //System.out.println(numberSum);
+        System.out.println(sum);
 
+        fiveLetterWord();
+    }
+    public static void fiveLetterWord(){
+        Scanner input;
+        input = new Scanner(System.in);
+        String [] words = {"cheese", "fish", "stars", "brain", "moon", "to", "I","cat", "murgatroyd"};
+        ArrayList<String> wordList = new ArrayList<>();
 
+        for (String word : words){
+            wordList.add(word);
+        }
+        //System.out.println(wordList);
+        System.out.println("Enter length of the word you want printed.");
+        Integer desiredLength = input.nextInt();
+        input.close();
+        for (String word: wordList){
+            if (word.length() == desiredLength){
+                System.out.println(word);
+            }
+        }
     }
 }
